ab203b851755860614446f10660ba19ca52c9720
Mamme-075 <mammeblink@gmail.com>
2025-01-01 18:48:52 +0300

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace LoginFormAssesment
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.firstNameBox = new System.Windows.Forms.TextBox();
+            this.lastNameBox = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.typeBox = new System.Windows.Forms.ComboBox();
+            this.loginLink = new System.Windows.Forms.LinkLabel();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.logInPanel = new System.Windows.Forms.Panel();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.loginPasswordBox = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.registerLink = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.registerPanel = new System.Windows.Forms.Panel();
+            this.logInPanel.SuspendLayout();
+            this.registerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Age :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(131, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Type :";
+            // 
+            // firstNameBox
+            // 
+            this.firstNameBox.Location = new System.Drawing.Point(268, 118);
+            this.firstNameBox.Name = "firstNameBox";
+            this.firstNameBox.Size = new System.Drawing.Size(286, 22);
+            this.firstNameBox.TabIndex = 4;
+            // 
+            // lastNameBox
+            // 
+            this.lastNameBox.Location = new System.Drawing.Point(268, 168);
+            this.lastNameBox.Name = "lastNameBox";
+            this.lastNameBox.Size = new System.Drawing.Size(286, 22);
+            this.lastNameBox.TabIndex = 5;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(268, 211);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(286, 22);
+            this.dateTimePicker.TabIndex = 6;
+            // 
+            // typeBox
+            // 
+            this.typeBox.FormattingEnabled = true;
+            this.typeBox.Location = new System.Drawing.Point(268, 267);
+            this.typeBox.Name = "typeBox";
+            this.typeBox.Size = new System.Drawing.Size(286, 24);
+            this.typeBox.TabIndex = 7;
+            // 
+            // loginLink
+            // 
+            this.loginLink.AutoSize = true;
+            this.loginLink.Location = new System.Drawing.Point(683, 47);
+            this.loginLink.Name = "loginLink";
+            this.loginLink.Size = new System.Drawing.Size(40, 16);
+            this.loginLink.TabIndex = 8;
+            this.loginLink.TabStop = true;
+            this.loginLink.Text = "LogIn";
+            this.loginLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.loginLink_LinkClicked);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(360, 321);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(81, 28);
+            this.saveBtn.TabIndex = 9;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            // 
+            // logInPanel
+            // 
+            this.logInPanel.Controls.Add(this.label6);
+            this.logInPanel.Controls.Add(this.label5);
+            this.logInPanel.Controls.Add(this.registerLink);
+            this.logInPanel.Controls.Add(this.loginBtn);
+            this.logInPanel.Controls.Add(this.loginPasswordBox);
+            this.logInPanel.Controls.Add(this.usernameBox);
+            this.logInPanel.Location = new System.Drawing.Point(0, 0);
+            this.logInPanel.Name = "logInPanel";
+            this.logInPanel.Size = new System.Drawing.Size(802, 502);
+            this.logInPanel.TabIndex = 10;
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(268, 140);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(249, 22);
+            this.usernameBox.TabIndex = 0;
+            // 
+            // loginPasswordBox
+            // 
+            this.loginPasswordBox.Location = new System.Drawing.Point(268, 196);
+            this.loginPasswordBox.Name = "loginPasswordBox";
+            this.loginPasswordBox.Size = new System.Drawing.Size(249, 22);
+            this.loginPasswordBox.TabIndex = 1;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(340, 239);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(101, 35);
+            this.loginBtn.TabIndex = 2;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // registerLink
+            // 
+            this.registerLink.AutoSize = true;
+            this.registerLink.Location = new System.Drawing.Point(357, 294);
+            this.registerLink.Name = "registerLink";
+            this.registerLink.Size = new System.Drawing.Size(58, 16);
+            this.registerLink.TabIndex = 3;
+            this.registerLink.TabStop = true;
+            this.registerLink.Text = "Register";
+            this.registerLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.registerLink_LinkClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(148, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "user name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(148, 201);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "password:";
+            // 
+            // registerPanel
+            // 
+            this.registerPanel.Controls.Add(this.loginLink);
+            this.registerPanel.Controls.Add(this.saveBtn);
+            this.registerPanel.Controls.Add(this.typeBox);
+            this.registerPanel.Controls.Add(this.dateTimePicker);
+            this.registerPanel.Controls.Add(this.firstNameBox);
+            this.registerPanel.Controls.Add(this.lastNameBox);
+            this.registerPanel.Controls.Add(this.label4);
+            this.registerPanel.Controls.Add(this.label3);
+            this.registerPanel.Controls.Add(this.label2);
+            this.registerPanel.Controls.Add(this.label1);
+            this.registerPanel.Location = new System.Drawing.Point(0, 0);
+            this.registerPanel.Name = "registerPanel";
+            this.registerPanel.Size = new System.Drawing.Size(799, 499);
+            this.registerPanel.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 502);
+            this.Controls.Add(this.registerPanel);
+            this.Controls.Add(this.logInPanel);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.logInPanel.ResumeLayout(false);
+            this.logInPanel.PerformLayout();
+            this.registerPanel.ResumeLayout(false);
+            this.registerPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox firstNameBox;
+        private System.Windows.Forms.TextBox lastNameBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.ComboBox typeBox;
+        private System.Windows.Forms.LinkLabel loginLink;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Panel logInPanel;
+        private System.Windows.Forms.TextBox loginPasswordBox;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.LinkLabel registerLink;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel registerPanel;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginFormAssesment
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void registerLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            logInPanel.Visible = false;
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            log
+        }
+    }
+}
